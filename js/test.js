06516c522dd96ca466561bd0d3f4c477e447dca2
BillChabzz <chaby9.8@gmail.com>
2016-07-18 06:57:03 +0300

    opacity fix with autoAlpha

--- js/test.js
+++ js/test.js
@@ -8,10 +8,17 @@ var toggle = $(".open"),
        TweenMax.from(about, 1 ,{
            x : 1000,
            autoAlpha: 0
-           
+        
        });
         
     });
    
 });
 
+/*functional programming for toggle listener
+take a function with two parameters
+e.g since element and duration are frequent items
+e.g function toggleAnimation(id,frequency)
+funtion should hold a tween object and return the values
+tween object should have fallback
+*/
